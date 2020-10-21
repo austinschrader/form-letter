794b1e8a65159749e40708e345ccef28e75c0466
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 10:44:11 -0700

    Re-add the content

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$('#formOne').submit(function (event) {
+		const firstNameInput = $('input#first-name').val();
+		const middleNameInput = $('input#middle-name').val();
+		const lastNameInput = $('input#last-name').val();
+
+		$('.first-name-output').text(firstNameInput);
+		$('.middle-name-output').text(middleNameInput);
+		$('.last-name-output').text(lastNameInput);
+
+		$('#letter').show();
+
+		event.preventDefault();
+	});
+});
